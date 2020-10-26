d2d42f6b05f2155aa0bc813a7b395f59ccba968f
Jihyun-Jang <jhjang742@gmail.com>
2020-10-26 14:48:45 +0000

    fix(sidebar): OP-59 #comment CSS changed to styled component #time 20m
    Co-authored-by: Azizi-A <azizi.adeyemo@gmail.com>

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,23 +1,20 @@
 import React from 'react';
-import './sidebar.css';
 import logOut from '../../utils/logOut';
 import styled from 'styled-components';
 
 const Sidebar = () => {
-  const [sidebarWidth, setSidebarWidth] = React.useState('20vw');
+  const [sidebarWidth, setSidebarWidth] = React.useState('13rem');
   const collapse = () => setSidebarWidth('0');
   return (
-    <nav style={{ width: sidebarWidth }}>
-      <button className="closebtn" onClick={collapse}>
-        &times;
-      </button>
-      <a href="/">Home</a>
-      <a href="/lessons">Lessons</a>
-      <a href="/resources">Resources</a>
-      <a href="/calendar">Calendar</a>
-      <a href="/links">Useful Links</a>
+    <Nav style={{ width: sidebarWidth }}>
+      <CloseButton onClick={collapse}>&times;</CloseButton>
+      <Anchor href="/">Home</Anchor>
+      <Anchor href="/lessons">Lessons</Anchor>
+      <Anchor href="/resources">Resources</Anchor>
+      <Anchor href="/calendar">Calendar</Anchor>
+      <Anchor href="/links">Useful Links</Anchor>
       <LogoutButton onClick={logOut}>Logout</LogoutButton>
-    </nav>
+    </Nav>
   );
 };
 
@@ -36,28 +33,61 @@ const LogoutButton = styled.button`
   font-family: 'Montserrat', sans-serif;
   background-color: #fff;
   border: none;
-  box-shadow: 4px 4px 0 hsl(0, 0%, 50.588235294117645%);
-  max-width: 120px;
+  box-shadow: 4px 4px 0 hsl(0, 0%, 50.6%);
+  max-width: 10rem;
   cursor: pointer;
-  padding: 10px;
-  margin: 2rem;
-  border-radius: 4px;
-  font-size: 15px;
+  padding: 0.5rem 1rem;
+  margin: 1rem 2rem;
+  border-radius: 10%;
+  font-size: 1.5em;
   font-weight: 900;
   &:hover {
     background: hsl(208, 99%, 69%);
   }
 `;
 
-// const Nav = styled.nav`
-//   height: 100%; /* 100% Full-height */
-//   width: 0; /* 0 width - change this with JavaScript */
-//   position: fixed; /* Stay in place */
-//   z-index: 1; /* Stay on top */
-//   top: 0;
-//   left: 0;
-//   background-color: #111; /* Black*/
-//   overflow-x: hidden; /* Disable horizontal scroll */
-//   padding-top: 60px; /* Place content 60px from the top */
-//   transition: 0.5s; /* 0.5 second transition effect to slide in the sidebar */
-// `;
+const Nav = styled.nav`
+  height: 100%; /* 100% Full-height */
+  width: 0; /* 0 width - change this with JavaScript */
+  position: fixed; /* Stay in place */
+  z-index: 1; /* Stay on top */
+  top: 0;
+  left: 0;
+  background-color: #111; /* Black*/
+  overflow-x: hidden; /* Disable horizontal scroll */
+  padding-top: 4rem; /* Place content 60px from the top */
+  transition: 0.5s; /* 0.5 second transition effect to slide in the sidebar */
+`;
+
+/* Position and style the close button (top right corner) */
+const CloseButton = styled.button`
+  position: absolute;
+  top: 0;
+  right: 1rem;
+  font-size: 3rem;
+  color: hsl(0, 0%, 50.6%);
+  background-color: none;
+`;
+
+/* The sidebar links */
+const Anchor = styled.a`
+  padding: 8px 8px 8px 32px;
+  text-decoration: none;
+  font-size: 25px;
+  color: hsl(0, 0%, 50.6%);
+  display: block;
+  transition: 0.3s;
+  /* When you mouse over the navigation links, change their color */
+  &:hover {
+    color: #f1f1f1;
+  }
+`;
+
+// @media screen and (max-height: 450px) {
+//   nav {
+//     padding-top: 15px;
+//   }
+//   nav a {
+//     font-size: 18px;
+//   }
+// }
