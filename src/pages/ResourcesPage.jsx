c44223c3b122f36a199b9ea79cce0743c4a994c8
Azizi Adeyemo <azizi.adeyemo@gmail.com>
2020-10-26 16:53:01 +0000

    feat(Resources page): OP-3 #comment initalised select level page #time 10m
    Co-authored-by: LiCern <lisacernilogar@me.com>

---
+++ src/pages/ResourcesPage.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+
+export default function ResourcesPage() {
+  return (
+    <form>
+      <h2>What level is your student at</h2>
+      <ul>
+        <li>
+          <p>A1 - complete beginner</p>
+        </li>
+        <li>
+          <p>A2 - elementary</p>
+        </li>
+        <li>
+          <p>B1 - intermediate</p>
+        </li>
+        <li>
+          <p>B2 - upper intermediate</p>
+        </li>
+        <li>
+          <p>C1 - advanced</p>
+        </li>
+        <li>
+          <p>C2 - proficient</p>
+        </li>
+      </ul>
+    </form>
+  );
+}
