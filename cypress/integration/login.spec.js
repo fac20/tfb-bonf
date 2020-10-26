76b4e00aabe71f716a1242c962030dda09d7ab79
Jihyun-Jang <jhjang742@gmail.com>
2020-10-26 10:59:19 +0000

    Cypress setting up

---
+++ cypress/integration/login.spec.js
@@ -0,0 +1,20 @@
+/// <reference types="cypress" />
+
+describe("Login page Test", () => {
+	it("Gets, types and asserts", () => {
+		// deletes the authenticated token from the indexedDB
+		indexedDB.deleteDatabase("firebaseLocalStorageDb");
+		cy.visit("http://localhost:3000");		
+		// Get an input, type into it and verify that the value has been updated
+		cy.get("input[type=email]")
+			.type("hello@me.com")
+            .should("have.value", "hello@me.com");
+        cy.get("input[type=password]")
+            .type("123456")
+            .should("have.value", "123456");
+        cy.get("button[type=submit]")
+            .click()
+        cy.contains("Home");
+               
+	});
+});
