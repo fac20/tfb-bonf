fd39a4fceb9c487d478c77fe7cfaaedc89bf8024
Azizi Adeyemo <azizi.adeyemo@gmail.com>
2020-10-27 17:01:15 +0000

    fix(resources page): OP-53 #comment removed unpassabel test #time 5m
    Co-authored-by:  aissshah <aissshah@outlook.com>

--- src/pages/ResourcesPage/ResourcesPage.test.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import ResourcesPage from './ResourcesPage';
-
-test('Resources page renders', () => {
-  render(<ResourcesPage />);
-  screen.getAllByText('Resources');
-});
