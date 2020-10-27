4cadf990fff0fa24bf8a67aeca414d9309b86a13
Azizi Adeyemo <azizi.adeyemo@gmail.com>
2020-10-27 15:08:39 +0000

    feat(resources page): OP-3 #comment db query started #time 1h 17m
    Co-authored-by: LiCern <lisacernilogar@me.com>

--- src/pages/ResourcesPage/SkillSelectionPage.jsx
+++ src/pages/ResourcesPage/SkillSelectionPage.jsx
@@ -1,8 +1,24 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useParams, Link } from 'react-router-dom';
+import { db } from '../../connection.js';
 
 export default function SkillSelectionPage() {
   const { level, skill } = useParams();
+
+  useEffect(() => {
+    db.collection('resources')
+      .where('level', '==', level.toUpperCase())
+      // .where(skill, '==', 'true')
+      .get()
+      .then((querySnapshot) =>
+        querySnapshot.forEach((doc) => {
+          console.log('Document data:', doc.data());
+        })
+      )
+      .catch((error) => {
+        console.log('Error getting document:', error);
+      });
+  });
 
   return (
     <>
@@ -26,7 +42,7 @@ export default function SkillSelectionPage() {
       </ul>
       {skill ? (
         <div>
-          <h1>Lesson data placeholder</h1>
+          <h1>Lesson data placeholder for {skill}</h1>
         </div>
       ) : null}
     </>
