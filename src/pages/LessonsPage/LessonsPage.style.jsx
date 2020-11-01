99ae7fbb54151c4af6490a460230cc37aaa5d839
Azizi Adeyemo <azizi.adeyemo@gmail.com>
2020-11-01 23:15:40 +0000

    refactor(LessonsPage): OP-6 #comment exported styles

---
+++ src/pages/LessonsPage/LessonsPage.style.jsx
@@ -0,0 +1,21 @@
+import styled from 'styled-components';
+
+export const LessonsWrapper = styled.div`
+  border-radius: 20px;
+  background-color: hsl(172, 87%, 91%);
+  box-shadow: 0 5px 25px hsla(0, 0%, 0%, 0.5);
+  width: fit-content;
+  margin: auto;
+  padding: 2.3rem 3.5rem;
+  border: 1px solid black;
+  box-shadow: 4px 4px 0 black;
+`;
+
+export const H2 = styled.h2`
+  text-align: center;
+`;
+
+export const Button = styled.button`
+  display: inline-block;
+  margin: 15px 10px 10px 0;
+`;
